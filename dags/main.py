b915e810ae25db17da754c6deea36a201b746633
caiosgon3 <caiosgon3@gmail.com>
2020-05-16 13:55:04 -0300

    Job properties in Airflow task

--- dags/main.py
+++ dags/main.py
@@ -12,34 +12,44 @@ default_args = {
 dag = DAG(
     'submit_job', default_args=default_args, schedule_interval="@once")
 
+project_id = 'sincere-bongo-264115'
+region = 'southamerica-east1'
+zone = 'southamerica-east1-a'
+cluster_name = 'airflow-cluster'
+storage_bucket = 'staging.sincere-bongo-264115.appspot.com'
+job_file = 'gs://teste-caio/movie_ratings/jobs/daily_job.py'
+properties = {"spark.jars.packages": "io.delta:delta-core_2.11:0.5.0"}
+
 t1 = DataprocClusterCreateOperator(
     task_id="create_cluster",
     gcp_conn_id='google_cloud_default',
-    project_id='sincere-bongo-264115',
-    region='southamerica-east1',
-    zone='southamerica-east1-a',
-    cluster_name="airflow-cluster",
-    storage_bucket='staging.sincere-bongo-264115.appspot.com',
+    project_id=project_id,
+    region=region,
+    zone=zone,
+    cluster_name=cluster_name,
+    storage_bucket=storage_bucket,
     num_workers=0,
     master_machine_type='n1-standard-2',
     dag=dag)
 
+
 t2 = DataProcPySparkOperator(
     task_id="run",
     gcp_conn_id='google_cloud_default',
-    project_id='sincere-bongo-264115',
-    main="gs://teste-caio/movie_ratings/jobs/daily_job.py",
+    project_id=project_id,
+    main=job_file,
     job_name='test',
-    cluster_name="airflow-cluster",
-    region='southamerica-east1',
+    dataproc_spark_properties=properties,
+    cluster_name=cluster_name,
+    region=region,
     dag=dag)
 
 
 t3 = DataprocClusterDeleteOperator(
     task_id='delete_cluster',
-    project_id='sincere-bongo-264115',
-    cluster_name='airflow-cluster',
-    region='southamerica-east1',
+    project_id=project_id,
+    cluster_name=cluster_name,
+    region=region,
     dag=dag)
 
 t1 >> t2 >> t3
